db2ccf3fe4264e5ad5881658465e5e916edd7b3f
Andrew Mason <mason@yelp.com>
2018-07-26 10:37:49 -0700

    Add basic parser to take a module, then collect files, with tests

--- colorwheel/main.py
+++ colorwheel/main.py
@@ -1,5 +1,31 @@
-def main():
+import argparse
+import sys
+from pathlib import Path
+
+
+def collect_files(path):
+    if path.is_file():
+        return [path]
+
+    return path.glob('**/*.py')
+
+
+def build_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('module')
+    return parser
+
+
+def _main(argv):
+    parser = build_parser()
+    parsed_args = parser.parse_args(argv)
+    files = collect_files(Path(parsed_args.module))
+    print(list(files))
     return 0
+
+
+def main():
+    sys.exit(_main(sys.argv[1:]))
 
 
 if __name__ == '__main__':
--- tests/main_test.py
+++ tests/main_test.py
@@ -1,5 +1,44 @@
+from pathlib import Path
+
 from colorwheel import main
 
 
+class TestCollectFiles(object):
+
+    def test_single_file(self, tmpdir):
+        tmpdir.join('foo.py').write('')
+        files = main.collect_files(Path(tmpdir.join('foo.py')))
+        assert files == [tmpdir.join('foo.py')]
+
+    def test_module(self, tmpdir):
+        tmpdir.join('foo.py').write('')
+        files = list(main.collect_files(Path(tmpdir)))
+        assert files == [tmpdir.join('foo.py')]
+
+    def test_module_python_only(self, tmpdir):
+        for ext in ['rb', 'py']:
+            tmpdir.join('foo.{ext}'.format(ext=ext)).write('')
+
+        files = list(main.collect_files(Path(tmpdir)))
+        assert Path(tmpdir.join('foo.py')) in files
+        assert Path(tmpdir.join('foo.rb')) not in files
+
+    def test_module_deeper_nesting(self, tmpdir):
+        tmpdir.join('foo.py').write('')
+        tmpdir.mkdir('bar').join('foo.py').write('')
+        tmpdir.join('bar').join('foo.rb').write('')
+
+        files = list(main.collect_files(Path(tmpdir)))
+        assert Path(tmpdir.join('foo.py')) in files
+        assert Path(tmpdir.join('bar').join('foo.py')) in files
+        assert Path(tmpdir.join('bar').join('foo.rb')) not in files
+
+
+def test_parser():
+    parser = main.build_parser()
+    parsed_args = parser.parse_args(['foo'])
+    assert parsed_args.module == 'foo'
+
+
 def test_main():
-    assert main.main() == 0
+    assert main._main(['']) == 0
